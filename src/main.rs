8b4ad7e49db849b446fafe71afebaba0fb742744
Raksan Ibatullin <raksan.ibatullin@toptal.com>
2024-05-13 22:44:28 -0300

    Read arguments provided

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::env;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let search_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file '{}'", search_path);
 }

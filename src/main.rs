30b46a1577b055c8697128d6bafde813c5885368
Raksan Ibatullin <raksan.ibatullin@toptal.com>
2024-05-13 23:08:16 -0300

    Extract config parsing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,31 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let search_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file '{}'", search_path);
+    println!("Searching for {}", config.query);
+    println!("In file '{}'", config.search_path);
 
-    let contents = fs::read_to_string(search_path)
+    let contents = fs::read_to_string(config.search_path)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
+
+struct Config {
+    query: String,
+    search_path: String,
+}
+
+impl Config {
+    fn new(args:&[String])->Config {
+        let query = &args[1];
+        let path = &args[2];
+    
+        Config {
+            query: query.clone(),
+            search_path: path.clone(),
+        }
+    }
+}
+
